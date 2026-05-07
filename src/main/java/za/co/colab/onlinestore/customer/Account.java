b04d7f8788e2b61d4ff62f74fe8f96b23cec08cb
atlegang-dotcom <atlegang.finiks@gmail.com>
2026-05-07 09:48:28 +0200

    Added Customer and Account class

--- src/main/java/za/co/colab/onlinestore/customer/Account.java
+++ src/main/java/za/co/colab/onlinestore/customer/Account.java
@@ -1,4 +1,31 @@
 package za.co.colab.onlinestore.customer;
 
 public class Account {
+    private final Customer user;
+    private int amount;
+
+    public Account(Customer user, int amount) {
+        this.user = user;
+        this.amount = amount;
+    }
+
+    public int amount() { return amount; }
+
+    public Customer user() { return user; }
+
+    public void deposit(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount cannot be less than or equal to zero");
+        }
+        // if we integrate a payment api, we can send the money from the users registered account
+        this.amount += amount;
+    }
+
+    public void withdraw(int amount) {
+        if ((this.amount - amount) < 0) {
+            throw new IllegalArgumentException("Withdrawal amount exceeds account balance");
+        }
+        // if we integrate a payment api, we can send the money back to the users registered account
+        this.amount -= amount;
+    }
 }
--- src/main/java/za/co/colab/onlinestore/customer/Customer.java
+++ src/main/java/za/co/colab/onlinestore/customer/Customer.java
@@ -1,4 +1,14 @@
 package za.co.colab.onlinestore.customer;
 
 public class Customer {
+    private final String name;
+    private final int customerId;
+
+    public Customer(String name, int customerId) {
+        this.name = name;
+        this.customerId = customerId;
+    }
+
+    public String name() { return name; }
+    public int customerId() { return customerId; }
 }
